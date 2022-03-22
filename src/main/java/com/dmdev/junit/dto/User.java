ec7e70fb38c1d756b91f2eca7672b09ae4777d47
artifact911 <artifact911@gmail.com>
2022-03-22 22:13:21 +0300

    lesson6 done. TDD

--- src/main/java/com/dmdev/junit/dto/User.java
+++ src/main/java/com/dmdev/junit/dto/User.java
@@ -1,5 +1,13 @@
 package com.dmdev.junit.dto;
 
+import lombok.Value;
+
+@Value(staticConstructor = "of")
 public class User {
+
+    Integer id;
+    String name;
+    String password;
+
 
 }
--- src/test/java/com/dmdev/junit/service/UserServiceTest.java
+++ src/test/java/com/dmdev/junit/service/UserServiceTest.java
@@ -8,12 +8,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 // @TestInstance(TestInstance.Lifecycle.PER_METHOD) // по умолчанию. Инстанс создается для каждого метода и требует BeforeAll и AfterAll в static
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceTest {
+
+    private static final User PETR = User.of(2, "Petr", "111");
+    private static final User IVAN = User.of(1, "Ivan", "123");
 
     private UserService userService;
 
@@ -38,10 +43,41 @@ public class UserServiceTest {
     @Test
     void usersSizeIfUserAdded() {
         System.out.println("Test 2: " + this);
-        userService.add(new User());
-        userService.add(new User());
+        userService.add(IVAN);
+        userService.add(PETR);
         var users = userService.getAll();
         assertEquals(2, users.size());
+    }
+
+    @Test
+    void loginSuccessIfUserExist() {
+        System.out.println("Test 3: " + this);
+        userService.add(IVAN);
+
+        Optional<User> mayBeUser = userService.login(IVAN.getName(), IVAN.getPassword());
+
+        assertTrue(mayBeUser.isPresent());
+        mayBeUser.ifPresent(user -> assertEquals(IVAN, user));
+    }
+
+    @Test
+    void loginFailIfPasswordIsNotCorrect() {
+        System.out.println("Test 4: " + this);
+        userService.add(IVAN);
+
+        var mayBeUser = userService.login(IVAN.getName(), "4434");
+
+        assertTrue(mayBeUser.isEmpty());
+    }
+
+    @Test
+    void loginFailIfUserDosNotExist() {
+        System.out.println("Test 5: " + this);
+        userService.add(IVAN);
+
+        var mayBeUser = userService.login("roy", IVAN.getPassword());
+
+        assertTrue(mayBeUser.isEmpty());
     }
 
     @AfterEach
